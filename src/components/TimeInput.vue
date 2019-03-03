d204a36a1a23f9c3e96e481a2443ed45e242f555
Anthony Collins <a.r.c_@hotmail.co.uk>
2019-03-03 13:45:18 +0000

    TimeInput component with tests

---
+++ src/components/TimeInput.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <input type="number" name="minutes" min="0" max="60" step="5" class="tq-time-input tq-time-input--minutes" v-bind:class="inputClasses" v-model.number="minutesInputValue">
+        <input type="number" name="hours" min="0" max="25" step="1" class="tq-time-input tq-time-input--hours" v-bind:class="inputClasses" v-model.number="hoursInputValue">
+        <input type="number" name="days" min="0" max="30" step="1" class="tq-time-input tq-time-input--days" v-bind:class="inputClasses" v-model.number="daysInputValue">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TimeInput',
+    props: ['guess'],
+    data() {
+        return {
+            inputClasses: 'shadow appearance-none border border-red py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline',
+            minutesInputValue: 0,
+            hoursInputValue: 0,
+            daysInputValue: 0
+        }
+    },
+    computed: {
+        totalMinutes: function(){
+            return this.minutesInputValue + ( this.hoursInputValue*60 ) + ( ( 60*24 )*this.daysInputValue )
+        }
+    }
+}
+</script>
+
---
+++ tests/unit/timeinput.spec.js
@@ -0,0 +1,48 @@
+import { shallowMount } from '@vue/test-utils'
+import TimeInput from '@/components/TimeInput.vue'
+
+describe('TimeInput.vue', () => {
+  it('displays inputs for minutes, hours, days', () => {
+    const wrapper = shallowMount(TimeInput)
+    const minutesInput = wrapper.find('input.tq-time-input--minutes')
+    expect(minutesInput.exists()).toBe(true)
+    const hoursInput = wrapper.find('input.tq-time-input--hours')
+    expect(hoursInput.exists()).toBe(true)
+    const daysInput = wrapper.find('input.tq-time-input--days')
+    expect(daysInput.exists()).toBe(true)
+  })
+  it('inputs convert total minutes to minutes, hours, days', () => {
+    const wrapper = shallowMount(TimeInput, {
+      propsData: {
+          guess: 0
+      }
+    })
+
+    // check initial values
+    expect(wrapper.vm.$props.guess).toBe(0)
+    expect(wrapper.vm.$data.minutesInputValue).toBe(0)
+    expect(wrapper.vm.$data.hoursInputValue).toBe(0)
+    expect(wrapper.vm.$data.daysInputValue).toBe(0)
+
+    // collect inputs
+    const minutesInput = wrapper.find('input.tq-time-input--minutes')
+    const hoursInput = wrapper.find('input.tq-time-input--hours')
+    const daysInput = wrapper.find('input.tq-time-input--days')
+
+    // set change events and new values on inputs
+    minutesInput.element.value = 5
+    hoursInput.element.value = 1
+    daysInput.element.value = 1
+    minutesInput.trigger('input')
+    expect(wrapper.vm.$data.minutesInputValue)
+    hoursInput.trigger('input')
+    expect(wrapper.vm.$data.hoursInputValue)
+    daysInput.trigger('input')
+    expect(wrapper.vm.$data.daysInputValue)
+ 
+    // expect accurate total minutes value after input changes: 5mins = 5, 1hr = 60, 1day = 1440, total = 1505
+    setTimeout(() => {
+      expect(wrapper.vm.totalMinutes).toBe(1505) 
+    }, 3000) 
+  })
+})
