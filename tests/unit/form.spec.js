2071dd0929f0bca9ee61d7ae6f1418c2284e0f54
Anthony Collins <a.r.c_@hotmail.co.uk>
2019-03-09 10:22:05 +0000

    unfinished test

--- tests/unit/form.spec.js
+++ tests/unit/form.spec.js
@@ -1,10 +1,19 @@
 import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import Form from '@/components/Form.vue'
+import TimeInput from '@/components/TimeInput.vue'
 
 describe('Form.vue', () => {
   it('renders the form', () => {
     const wrapper = shallowMount(Form)
     expect(wrapper.find('form').exists()).toBe(true)
+  })
+  it('catches guessUpdated event emitted by inputs', () => {
+    const wrapper = mount(Form)
+    const input = wrapper.find(TimeInput)
+
+    TimeInput.$emit('guessUpdated')
+
   })
   it('emits custom estimation event on submission', () => {
     const wrapper = shallowMount(Form)
