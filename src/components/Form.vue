615f762bd77a91362e4a4f018eb2d4e79e2331ab
Anthony Collins <a.r.c_@hotmail.co.uk>
2019-03-31 09:41:08 +0100

    corrected passed guess props, change to strings

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,17 +1,17 @@
 <template>
 <div>
-  <form>
+  <form v-on:guessUpdated="updateGuesses">
     Optimistic:
     <br>
-    <TimeInput v-bind:guess="optimisticGuess" />
+    <TimeInput v-bind:guess="'optimisticGuess'" />
     <br>
     Best Guess:
     <br>
-    <TimeInput v-bind:guess="bestGuess" />
+    <TimeInput v-bind:guess="'bestGuess'" />
     <br>
     Pessimistic:
     <br>
-    <TimeInput v-bind:guess="pessitimisticGuess" />
+    <TimeInput v-bind:guess="'pessitimisticGuess'" />
     <br>
     <input type="submit" class="tq-submit bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded" value="Submit" v-on:click.prevent="emitGuessesSubmitted">
   </form>
