7a738ced83b86653ec36fa84efcbe7c5b2bec25f
Anthony Collins <a.r.c_@hotmail.co.uk>
2019-04-05 12:24:58 +0100

    corrected test and implementation for guess submission event

--- tests/unit/form.spec.js
+++ tests/unit/form.spec.js
@@ -10,7 +10,19 @@ describe('Form.vue', () => {
     const wrapper = shallowMount(Form)
     const submitButton = wrapper.find('input.tq-submit')
     const rootWrapper = createWrapper(wrapper.vm.$root)
+
+    wrapper.vm.$data.bestGuess = 5
+    wrapper.vm.$data.optimisticGuess = 5
+    wrapper.vm.$data.pessimisticGuess = 5
+
     submitButton.trigger('click')
+
+    console.log(rootWrapper.emitted().guessesSubmitted[0][0])
+
     expect(rootWrapper.emitted().guessesSubmitted).toBeTruthy()
+    expect(rootWrapper.emitted().guessesSubmitted[0][0].bestGuess).toEqual(5)
+    expect(rootWrapper.emitted().guessesSubmitted[0][0].optimisticGuess).toEqual(5)
+    expect(rootWrapper.emitted().guessesSubmitted[0][0].pessimisticGuess).toEqual(5)
+
   })
 })
