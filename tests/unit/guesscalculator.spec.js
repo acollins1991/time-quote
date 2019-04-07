c547477314b6532c2307a437a705a8fac156fdaa
Anthony Collins <a.r.c_@hotmail.co.uk>
2019-04-07 10:20:00 +0100

    guess calc component with tests added

--- tests/unit/guesscalculator.spec.js
+++ tests/unit/guesscalculator.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createWrapper } from '@vue/test-utils'
 import GuessCalculator from '@/components/GuessCalculator.vue'
 
 describe( 'GuessCalculator.vue', () => {
@@ -7,5 +7,50 @@ describe( 'GuessCalculator.vue', () => {
         expect(wrapper.find('span.minutes').text()).toEqual('0m')
         expect(wrapper.find('span.hours').text()).toEqual('0h')
         expect(wrapper.find('span.days').text()).toEqual('0d')
+    })
+    it('Calculates the estimation on guessSubmitted root emitted event using PERT formula, 5mins', () => {
+
+        const wrapper = shallowMount(GuessCalculator)
+        const rootWrapper = createWrapper(wrapper.vm.$root)
+
+        rootWrapper.vm.$emit('guessesSubmitted', { 'bestGuess': 5, 'optimisticGuess': 5, 'pessimisticGuess': 5 })
+
+        expect(rootWrapper.emitted().guessesSubmitted).toBeTruthy()
+
+        // 1505mins converts to 1day 1hours 5mins
+        expect(wrapper.vm.$data.minutes).toEqual(5)
+        expect(wrapper.vm.$data.hours).toEqual(0)
+        expect(wrapper.vm.$data.days).toEqual(0)
+
+    })
+    it('Calculates the estimation on guessSubmitted root emitted event using PERT formula, 1439mins', () => {
+
+        const wrapper = shallowMount(GuessCalculator)
+        const rootWrapper = createWrapper(wrapper.vm.$root)
+
+        rootWrapper.vm.$emit('guessesSubmitted', { 'bestGuess': 1439, 'optimisticGuess': 1439, 'pessimisticGuess': 1439 })
+
+        expect(rootWrapper.emitted().guessesSubmitted).toBeTruthy()
+
+        // 1505mins converts to 1day 1hours 5mins
+        expect(wrapper.vm.$data.minutes).toEqual(59)
+        expect(wrapper.vm.$data.hours).toEqual(23)
+        expect(wrapper.vm.$data.days).toEqual(0)
+
+    })
+    it('Calculates the estimation on guessSubmitted root emitted event using PERT formula, 1505mins', () => {
+
+        const wrapper = shallowMount(GuessCalculator)
+        const rootWrapper = createWrapper(wrapper.vm.$root)
+
+        rootWrapper.vm.$emit('guessesSubmitted', { 'bestGuess': 1505, 'optimisticGuess': 1505, 'pessimisticGuess': 1505 })
+
+        expect(rootWrapper.emitted().guessesSubmitted).toBeTruthy()
+
+        // 1505mins converts to 1day 1hours 5mins
+        expect(wrapper.vm.$data.minutes).toEqual(5)
+        expect(wrapper.vm.$data.hours).toEqual(1)
+        expect(wrapper.vm.$data.days).toEqual(1)
+
     })
 })
