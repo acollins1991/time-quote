e86d4934f44d91fdcd18e4ec1b3d0e53c40e87ed
Anthony Collins <a.r.c_@hotmail.co.uk>
2019-04-07 11:07:13 +0100

    Revert "attempt to resolve serviceworker error in ghpages"
    This reverts commit 1c58b2aef1a122967e5e8c236051f8c003f3c56f.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
+//import './registerServiceWorker'
 import '@/assets/css/tailwind.css'
 
 Vue.config.productionTip = false
